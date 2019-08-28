c4b4f52974cb212210a4502004879118526514e5
Christopher Tero <christopher.tero@gmail.com>
2019-08-28 11:56:17 -0500

    fixed labels in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Hymn.create(title: "The Solid Rock", description: "This song shows how Jesus Christ is our Solid Rock to lean upon, no matter what storms life throws our way", story: "Upcoming", editorRating: 5, editorReview: "One of the greatest hymns ever written", doctrinal: 3, scriptural: 5)
+Hymn.create(title: "The Solid Rock", description: "This song shows how Jesus Christ is our Solid Rock to lean upon, no matter what storms life throws our way", story: "Upcoming", editorRating: 5, editorReview: "One of the greatest hymns ever written", doctrine: 3, scriptural: 5)
+
+Resource.create(name: "Some hymn history book", url: "none yet, affiliate link incoming", description: "Some great book workth buying", price: "Some low price")
